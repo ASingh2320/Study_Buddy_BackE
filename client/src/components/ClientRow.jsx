5dcb62a1a7f4b0c2e9fa87538ee25aaa3690e091
Anmol Singh <ASingh2320@outlook.com>
2023-02-12 03:56:46 -0500

    first commit

---
+++ client/src/components/ClientRow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ClientRow({client }) {
+  return (
+    <tr>
+        <td>{client.name}</td>
+        <td>{client.email}</td>
+        <td>{client.phone}</td>
+        <td>
+            <button className='btn btn-danger btn-sm'>Delete</button>
+        </td>
+    </tr>
+  )
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav className='navbar bg-light mb-4 p-0'>
+        <div className='container'>
+            <a className='navbar-brand' href='/'>
+                <div className='d-flex'>
+                    <h1>Project Managment</h1>
+                </div>
+            </a>
+        </div>
+    </nav>
+  )
+}
+
